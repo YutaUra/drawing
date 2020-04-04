a56049ae502a6d467d33c63c500b1a281faf0a6f
yuta-ura <yuta.ura@quipper.com>
2020-04-05 01:26:26 +0900

    ok

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,8 +13,7 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: () => import('@/views/Home.vue'),
-    meta: { requireAuth: true }
+    component: () => import('@/views/Home.vue')
   },
   {
     path: '/test',
